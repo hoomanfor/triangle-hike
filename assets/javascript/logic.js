ab61e14e7cab4815deb2c79b49da076a128105f2
Hoo Man <hoomanfor@gmail.com>
2019-11-03 12:05:37 -0500

    trail and forecast redesign

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -3,16 +3,21 @@ $.ajax({
     url: "https://api.openweathermap.org/data/2.5/forecast?lat=35.867542&lon=-78.752154&units=imperial&appid=c3d8318715b5794788759512c752b645",
     method: "GET"
 }).then(function(response) {
+    var index = 0; 
     var forecastArr = response.list;
     console.log("forecastArr", forecastArr);
     forecastArr.forEach(function(element) {
-        console.log("element.dt", element.dt);
+        // console.log("element.dt", element.dt);
         var dateTime = moment.unix(element.dt);
         var hour = parseInt(dateTime.format("H"));
-        // if (dateTime.display)
-        console.log("hour", hour);
-        if (hour >= 7 && hour <= 21) {
-            console.log(dateTime.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+        // console.log("hour", hour);
+        if (index < 10 && hour >= 7 && hour <= 17) {
+            index++; 
+            console.log(element);
+            var dayOfWeek = dateTime.format("ddd").toUpperCase();
+            var time = dateTime.format("h:mm A")
+            console.log("dayOfWeek", dayOfWeek);
+            console.log("time", time);
         }
     })
     // var now = moment.unix(response.list[0].dt).format("dddd")
