8587a722a34621d24022e92fd0c43c2e1484e425
Hooman Foroudastan <hoomanfor@gmail.com>
2019-11-07 18:38:09 -0500

    added trailhead button and styled table columns

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -19,18 +19,45 @@ function initMap() {
         center: myLatLng,
         zoom: 15
     });
-$(document).on("click", ".parking-icon", function(event) {
+$(document).on("click", ".parking-btn", function(event) {
     map = new google.maps.Map(document.getElementById('map'), {
         center: myLatLng,
         mapTypeId: google.maps.MapTypeId.SATELLITE,
         zoom: 20
-    })
+    });
+
+    var infowindow = new google.maps.InfoWindow({
+        content: "35.867542, -78.752154"
+    });
+
     var marker = new google.maps.Marker({
         position: myLatLng,
-        icon: google.maps.Icon,
-        map: map,
-        title: 'Parking'
+        animation: google.maps.Animation.DROP,
+        map: map
         });
+        infowindow.open(map, marker);
+        marker.addListener("click", function() {
+        infowindow.open(map, marker);
+        })
+    })
+$(document).on("click", ".trailhead-btn", function(event) {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 35.87185, lng: -78.76085},
+        mapTypeId: google.maps.MapTypeId.SATELLITE,
+        zoom: 20
+    })
+    var infowindow = new google.maps.InfoWindow({
+        content: "35.87185, -78.76085"
+    })
+    var marker = new google.maps.Marker({
+        position: {lat: 35.87185, lng: -78.76085},
+        animation: google.maps.Animation.DROP,
+        map: map
+        });
+        infowindow.open(map, marker);
+        marker.addListener("click", function() {
+            infowindow.open(map, marker);
+        })
     })
 }
 
@@ -51,28 +78,28 @@ $.ajax({
 }).then(function(response) {
     var index = 0; 
     var forecastArr = response.list;
-    console.log("forecastArr", forecastArr);
+    // console.log("forecastArr", forecastArr);
     forecastArr.forEach(function(element) {
         // console.log("element.dt", element.dt);
         var dateTime = moment.unix(element.dt);
         var hour = parseInt(dateTime.format("H"));
         // console.log("hour", hour);
-        if (index < 12 && hour >= 7 && hour <= 17) {
+        if (index < 10 && hour >= 7 && hour <= 17) {
             index++; 
-            console.log(element);
+            // console.log(element);
             var dayOfWeek = dateTime.format("ddd").toUpperCase();
             var time = dateTime.format("hA");
             var temp = Math.round(element.main.temp);
             var icon = "<td><img class='img-fluid' src='http://openweathermap.org/img/wn/" + element.weather[0].icon + "@2x.png' height='70' width='70'></td>"
-            console.log("icon", icon);
-            console.log("temp", temp);
-            console.log("dayOfWeek", dayOfWeek);
-            console.log("time", time);
-            var row = $("<tr>");
+            // console.log("icon", icon);
+            // console.log("temp", temp);
+            // console.log("dayOfWeek", dayOfWeek);
+            // console.log("time", time);
+            var row = $("<tr class='forecast' unix='"+ element.dt + "'>");
             var td = $("<td>");
             td.html(dayOfWeek + " " + time + "<br>" + " " + temp + "&#8457;");
             row.append(td, icon); 
-            if (index < 7) {
+            if (index < 6) {
                 $("#forecast-col-1").append(row);
             } else {
                 $("#forecast-col-2").append(row);
@@ -80,6 +107,12 @@ $.ajax({
         }
     })
 })
+
+$(document).on("click", ".forecast", function(event) {
+    console.log("This Works!");
+    console.log($(this).attr("unix"));
+    //Research adding a Vertically Centered Modal to Confirm or Deny visiting a trial at the selected Date/Time
+});
 
 
 
